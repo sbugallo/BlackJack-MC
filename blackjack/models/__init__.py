c98114065a8653491e5f322649cedc1bfd3bfbf8
Sergio Bugallo Enjamio <dnamaster.22@gmail.com>
2019-09-02 20:26:06 +0200

    Add actions class and tests suite

--- blackjack/models/__init__.py
+++ blackjack/models/__init__.py
@@ -1,2 +1,3 @@
+from .action import Action  # noqa:F401
 from .card import Card  # noqa:F401
 from .deck import Deck  # noqa:F401
---
+++ tests/unit/models/test_action.py
@@ -0,0 +1,21 @@
+import pytest
+
+from blackjack.models import Action
+
+
+@pytest.mark.unit
+def test_action_correct_initialization():
+    assert Action(0) == Action.hit
+    assert Action(1) == Action.stand
+    assert Action(2) == Action.double_down
+    assert Action(3) == Action.split
+    assert Action(4) == Action.insurance
+
+
+@pytest.mark.unit
+def test_action_correct_values():
+    assert Action.hit.value == 0
+    assert Action.stand.value == 1
+    assert Action.double_down.value == 2
+    assert Action.split.value == 3
+    assert Action.insurance.value == 4
